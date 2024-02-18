10067adba3009713748d1f029e51a891f620fd26
Mike Rushton <mrush94ton@gmail.com>
2024-02-18 21:00:06 +0000

    created login api route and functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/userModel.js"
 import bcryptjs from 'bcryptjs'
 import { errorHandler } from "../middleware/error.js"
+import jwt from 'jsonwebtoken'
 
 const createUser = async (req, res, next) => {
     const { username, email, password } = req.body
@@ -33,4 +34,33 @@ const createUser = async (req, res, next) => {
     }
 }
 
-export {createUser}
+const loginUser = async (req, res, next) => {
+    const { email, password } = req.body
+    if (!email || !password || email === '' || password === '') {
+        next(errorHandler(400, 'All fields are required'))
+    }
+
+    try {
+        const validUser = await User.findOne({email})
+        if (!validUser) {
+            return next(errorHandler(404, 'User not found'))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if (!validPassword) {
+            return next(errorHandler(400, 'Invalid password'))
+        }
+        const token = jwt.sign({
+            id: validUser._id, isAdmin: validUser.isAdmin
+        }, process.env.VITE_JWT_TOKEN)
+
+        const { password: pass, ...rest} = validUser._doc //seperates the password from the rest of the json data
+
+        res.status(200).cookie('access_token', token, {
+            httpOnly: true
+        }).json(rest)
+    } catch (error) {
+        next(error)
+    }
+}
+
+export {createUser, loginUser}
